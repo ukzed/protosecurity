2facdab868ca2736ac8f60a5690d0ae7122114f8
hchin3 <48957464+hchin3@users.noreply.github.com>
2021-02-22 10:25:01 -0500

    PROTO-14 Initialise security service

---
+++ src/main/java/com/zirakzigil/protosecurity/ProtosecurityApplication.java
@@ -0,0 +1,13 @@
+package com.zirakzigil.protosecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtosecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtosecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zirakzigil/protosecurity/ProtosecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zirakzigil.protosecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtosecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
